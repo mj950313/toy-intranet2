b21498c7fdf684788f513d4caccf3af562663a8e
김민재 <kimminjae@gimminjaeui-MacBookPro.local>
2024-03-08 13:12:59 +0900

    calendar agenda delete

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -1,22 +1,25 @@
-import 'react-calendar/dist/Calendar.css';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment'
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useSelector } from "react-redux";
+import { StateType } from "../types/user";
 
-const localizer = momentLocalizer(moment)
+const localizer = momentLocalizer(moment);
 
-export default function CalendarPage () {
+export default function CalendarPage() {
+  const users = useSelector((state: StateType) => state.user.users);
 
   return (
     <div>
-        <div>
+      <div>
         <Calendar
           localizer={localizer}
           startAccessor="start"
           endAccessor="end"
-          style={{ height: 500 }}
+          style={{ height: 700 }}
+          views={["month", "week", "day"]}
         />
-        </div>
+      </div>
     </div>
   );
 }
